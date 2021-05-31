11d798bc321b31f31e558a638f021d813c6c7049
João Victor de Melo <j_v_melo@hotmail.com>
2021-05-31 10:56:22 -0300

    alteracao_layout
    Tentativa de criação do layout das telas.

--- client/src/App.js
+++ client/src/App.js
@@ -2,28 +2,30 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1> CADASTRAR ESTOQUE </h1>
-      <form>
-      <div class="form">
-        <div class="nome">
-          <label>Descrição: </label>
-          <input type="text" name="nomeProduto" id="nome"/>
-        </div>
-        <div class="preco">
-          <label>Preço:</label>
-          <input type="integer" name="precoProduto" id="preco"/>
-        </div>
-        <div class="estoque">
-        <label>Estoque:</label>
-        <input type="number" name="estoqueProduto" id="estoque" min="1" max="100"/>
+    <div classname="main">
+      <div className="header">
+        <h1> GERENCIAR ESTOQUE - REGISTROS </h1>
       </div>
-        <div class="button">
-          <input type="submit" value="confirma"/> 
-          <input type="reset"/>
+      <div className="background">
+        <div class="card">
+          <div class="nome">
+            <label>Descrição: </label>
+            <input type="text" name="nomeProduto" id="nome"/>
+          </div>
+          <div class="preco">
+            <label>Preço:</label>
+            <input type="integer" name="precoProduto" id="preco"/>
+          </div>
+          <div class="estoque">
+            <label>Estoque:</label>
+            <input type="number" name="estoqueProduto" id="estoque" min="1" max="100"/>
+          </div>
+          <div class="button">
+            <input type="submit" value="confirma"/> 
+            <input type="reset"/>
+          </div>
         </div>
       </div>
-      </form>
     </div>
   );
 }
--- client/src/api/produtos.js
+++ client/src/api/produtos.js
@@ -1,5 +1,13 @@
 import axios from 'axios';
 
+const api = axios.create({
+  baseURL: "http://localhost:3001/",
+})
+
+/* Tentativa de criação - Teste
 export default axios.create({
   baseURL: "http://localhost:3001/",
 });
+*/
+
+export default api;
